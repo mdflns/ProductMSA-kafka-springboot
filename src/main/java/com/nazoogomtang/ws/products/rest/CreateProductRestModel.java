29c3015c89c56a006000e21faaa3fff70cb946fe
John Doe <johndoe@on.net>
2026-08-18 23:05:34 +0900

    Controller init

---
+++ src/main/java/com/nazoogomtang/ws/products/rest/CreateProductRestModel.java
@@ -0,0 +1,14 @@
+package com.nazoogomtang.ws.products.rest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+public class CreateProductRestModel {
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
